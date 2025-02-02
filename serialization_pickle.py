48da5e9532372de9d1787b02416b8abedc6c1893
sujit1984 <sujitsugathan2002@gmail.com>
2025-02-02 12:23:15 +0530

    Serialization and Deserialization using Pickle

---
+++ serialization_pickle.py
@@ -0,0 +1,11 @@
+import pickle
+
+friends = {"Dan":[20, "London", 8923492232], "Maria": [30,"Madrid", 61232332]}
+
+with open("friends.dat", 'wb') as file:
+    pickle.dump(friends,file)
+
+with open("friends.dat", 'rb') as fr:
+    obj = pickle.load(fr)
+    print(type(obj))
+    print(obj)
